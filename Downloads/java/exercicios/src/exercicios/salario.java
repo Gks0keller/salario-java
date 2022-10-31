4534a68fe392a131ba29326a0c010f0169ba41d7
GABRIEL KOEHLER DA SILVA <gabriel_k_da-silva@estudante.sesisenai.org.br>
2022-10-31 14:07:49 -0300

    Projeto Java Acabado

---
+++ Downloads/java/exercicios/src/exercicios/salario.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class salario {
+	
+	static Scanner sc=new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		System.out.println("escolha uma opção: \n 1. Novo salário\n 2. Férias \n 3. Décimo terceiro \n 4. Sair  ");
+		
+		
+	}
+}
